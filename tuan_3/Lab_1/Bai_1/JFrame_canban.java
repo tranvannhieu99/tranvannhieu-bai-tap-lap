9f7fbb34ec04605c817214718a6e9429665cfede
= <=>
2019-09-29 23:21:07 +0700

    update tuan3

---
+++ tuan_3/Lab_1/Bai_1/JFrame_canban.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dhttmmt.tuan3;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DELL
+ */
+public class JFrame_canban extends JFrame{
+    public JFrame_canban(){
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new JFrame_canban();
+    }
+}
